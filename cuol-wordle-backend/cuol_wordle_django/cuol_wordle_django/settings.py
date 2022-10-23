7f8895e047195e66e974a74b476fde228978efea
Adam Kaczmarski <adam.kacz10@gmail.com>
2022-10-23 17:07:15 +0100

    started api app in django, changed settings to postgres

--- cuol-wordle-backend/cuol_wordle_django/cuol_wordle_django/settings.py
+++ cuol-wordle-backend/cuol_wordle_django/cuol_wordle_django/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cuol_wordle_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cuoldle',
+        'USER': 'backend',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT':'5432'
     }
 }
 
--- cuol-wordle-ui/cuol_wordle_ui/src/App.js
+++ cuol-wordle-ui/cuol_wordle_ui/src/App.js
@@ -4,11 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+qwewq          Edit <code>src/App.js</code> and save to reload.
         </p>
-          Learn React
+qwewq          Learn React
       </header>
     </div>
   );
